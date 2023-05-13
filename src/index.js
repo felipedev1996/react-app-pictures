2a5bf6d0ecf1952fd324dd361b4a589eb403d466
felipedev1996 <felipepino1996@gmail.com>
2023-05-13 17:38:51 -0400

    dotenv install

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 
 
-
+const api = process.env.API_TOKEN;
+console.log(api);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,14 +1,13 @@
 
 const API_URL = "https://api.themoviedb.org/3";
 
-export function getFetch(path) {
-
-   return fetch(API_URL + path, {
+export async function getFetch(path) {
+    const response = await fetch(API_URL + path, {
         headers: {
             Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NjM3YzVkZjYyMjExZWZmMGM3NDU5MmVlZWQyNTZiZiIsInN1YiI6IjY0MWE5NWYwMTQyZWYxMDBkOWJhZjQ5ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gj2xvmLc1o5LNT7OpRv6t3kCSeODnMBqQog--pncrNw",
-
             "Content-type": "application/json;charset=utf-8"
         }
-    }).then((result) => result.json())
+    });
+    const result = await response.json();
+    return result;
 }
-
