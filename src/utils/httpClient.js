286fa259d998c8b02a0efca577b186fc9d399fb0
felipedev1996 <felipepino1996@gmail.com>
2023-05-13 18:08:04 -0400

    configuracion de variables de entorno

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,6 +1,4 @@
-import {config} from 'dotenv';
- 
-console.log(config()); 
+
 
 
 
@@ -9,7 +7,7 @@ const API_URL = "https://api.themoviedb.org/3";
 export async function getFetch(path) {
     const response = await fetch(API_URL + path, {
         headers: {
-            Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NjM3YzVkZjYyMjExZWZmMGM3NDU5MmVlZWQyNTZiZiIsInN1YiI6IjY0MWE5NWYwMTQyZWYxMDBkOWJhZjQ5ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gj2xvmLc1o5LNT7OpRv6t3kCSeODnMBqQog--pncrNw",
+            Authorization: process.env.REACT_APP_API_TOKEN,
             "Content-type": "application/json;charset=utf-8"
         }
     });
